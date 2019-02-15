089ab4a87ee72666dc1204b98200961930a62f25
Zubair Tariq <zubair_success747@yahoo.com>
2019-02-15 14:16:00 +0200

    Added RESTful Services to the application

--- src/main/java/study/app/bookstore/domain/Book.java
+++ src/main/java/study/app/bookstore/domain/Book.java
@@ -7,6 +7,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 
 public class Book {
@@ -21,6 +23,7 @@ public class Book {
 	private double price;
 	
 	@ManyToOne
+	@JsonIgnore
 	@JoinColumn(name="categoryId")
 	private Category category;
 	
--- src/main/java/study/app/bookstore/web/BookController.java
+++ src/main/java/study/app/bookstore/web/BookController.java
@@ -1,11 +1,15 @@
 package study.app.bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import study.app.bookstore.domain.Book;
 import study.app.bookstore.domain.BookRepository;
@@ -33,6 +37,7 @@ public class BookController {
 	
 	}
 	
+	
 	@RequestMapping(value = "/editBook/{id}")
 	public String editBook(@PathVariable("id") Long bookId, Model model){
 	model.addAttribute("book", repository.findById(bookId));
@@ -54,6 +59,18 @@ public class BookController {
 	repository.deleteById(bookId);
 	return "redirect:../bookList";
 	}
+	
+	// RESTful service to get all Books
+    @RequestMapping(value="/getbooks", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {	
+        return (List<Book>) repository.findAll();
+    }    
+
+	// RESTful service to get Book by id
+    @RequestMapping(value="/getbooks/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {	
+    	return repository.findById(bookId);
+    }
 	
 
 }
